859b52b1d1caf63ff61dc49d7bcc6f984c7b9293
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-16 05:43:19 -0700

    Added WorksController#new and create

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -17,20 +17,20 @@ class WorksController < ApplicationController
   #   @trips = @driver.trips
   # end
 
-  # def new
-  #   @driver = Driver.new
-  # end
+  def new
+    @work = Work.new
+  end
 
-  # def create
-  #   @driver = Driver.new(driver_params) 
-  #   if @driver.save 
-  #     redirect_to driver_path(@driver)
-  #     return
-  #   else 
-  #     render :new 
-  #     return
-  #   end
-  # end
+  def create
+    @work = Work.new(work_params) 
+    if @work.save 
+      redirect_to work_path(@work)
+      return
+    else 
+      render :new 
+      return
+    end
+  end
 
   # def edit
   #   @driver = Driver.find_by(id: params[:id])
@@ -71,7 +71,7 @@ class WorksController < ApplicationController
 
   # private
 
-  # def driver_params
-  #   return params.require(:driver).permit(:name, :vin, :available)
-  # end
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
