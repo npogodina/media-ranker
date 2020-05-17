16faf8a0c600f77736db0caa33fc21757a03ec00
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-16 17:02:31 -0700

    Added validation for title and category in work model; wrote tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+
+    it 'is valid when all fields are present' do
+      work = Work.new(
+        category: "book",
+        title: "Floret Farm's Cut Flower Garden",
+        creator: "Erin Benzakein",
+        publication_year: 2017,
+        description: "Gardening Book for Beginners, Floral Design and Flower Arranging Book"
+      )
+      result = work.valid?
+      expect(result).must_equal true
+    end
+
+    it "is valid when just title and category are present" do
+      work = Work.new(
+      category: "book",
+      title: "Floret Farm's Cut Flower Garden"
+      )
+      result = work.valid?
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a title' do
+      work = Work.new(category: "book")
+      result = work.valid?
+      expect(result).must_equal false
+    end
+
+    it 'is invalid without a category' do
+      work = Work.new(title: "Floret Farm's Cut Flower Garden")
+      result = work.valid?
+      expect(result).must_equal false
+    end
+
+    it "is invalid if a work with the same title exists in the same category" do
+      poodr = Work.find_by(title: "Practical Object Oriented Design in Ruby")
+      duplicate = Work.new(
+        category: "book",
+        title: "Practical Object Oriented Design in Ruby"
+      )
+      result = duplicate.valid?
+      expect(result).must_equal false
+    end
+
+    it "is valid if a work with the same title exists in a different same category" do
+      poodr = Work.find_by(title: "Practical Object Oriented Design in Ruby")
+      exciting_movie = Work.new(
+        category: "movie",
+        title: "Practical Object Oriented Design in Ruby"
+      )
+      result = exciting_movie.valid?
+      expect(result).must_equal true
+    end
+  end
 end
