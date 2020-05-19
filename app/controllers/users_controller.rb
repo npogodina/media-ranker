ad9aec4045bc4c2a14815d29c790bda3209f0a4e
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-19 13:30:12 -0700

    Added UsersController#logout and changed navbar links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,20 @@ class UsersController < ApplicationController
     user = User.find_by(name: name)
     if user
       session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as returning user #{name}"
+      flash[:success] = "Successfully logged in as existing user #{name}"
     else
       user = User.create(name: name)
       session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as new user #{name}"
+      flash[:success] = "Successfully created new user #{name} with ID #{user.id}"
     end
   
+    redirect_to welcome_path
+    return
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
     redirect_to welcome_path
     return
   end
