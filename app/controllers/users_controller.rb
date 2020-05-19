e25311147705e0ab52e37b28d507fe8862866aac
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-19 14:56:13 -0700

    Added UsersController#index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all.order(:id)
+  end
+
+  def show
+
+  end
+
   def login_form
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :works
+  
   resource :welcome, only: :show
-
   root to: "welcomes#show", as: "welcome_path"
 
+  resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
