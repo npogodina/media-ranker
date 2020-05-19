964285f03e103ed3072c2a8d3d81c4d4f840b06b
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-19 09:26:41 -0700

    Added UsersController#login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def login_form
     @user = User.new
   end
+
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{name}"
+    else
+      user = User.create(name: name)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{name}"
+    end
+  
+    redirect_to welcome_path
+    return
+  end
 end
