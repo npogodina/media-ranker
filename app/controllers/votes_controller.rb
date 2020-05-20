32cbfb23933a606a44f92ba9ae593d7fd9362f17
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-20 08:32:22 -0700

    Generated VotesController, added #create and set up toutes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,29 @@
+class VotesController < ApplicationController
+  def create
+    user = User.find_by(id: session[:user_id])
+    if user.nil?
+      flash[:error] = "A problem occured. You must log in to do that." 
+      redirect_back(fallback_location: root_path)
+      return
+    end
+    # if voted already? (has many-through relationship)
+
+    work = Work.find_by(id: params[:work_id])
+    if work.nil?
+      flash[:error] = "A problem occured. We couldn't find this work."
+      redirect_back(fallback_location: root_path)
+      return
+    end
+
+    @vote = Vote.new(
+      user_id: user.id,
+      work_id: work.id
+    )
+
+    if @vote.save
+      flash[:success] = "Successfully upvoted!" 
+    else
+      flash.now[:error] = "A problem occured. Could not create register vote."
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
+
+  resources :votes, only: :create
 end
