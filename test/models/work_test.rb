2e5ad8b5337889885494bf4f91864b3f056f4d5e
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-21 15:32:07 -0700

    Added tests for Work.top_ten and Work.spotlight

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -92,7 +92,7 @@ describe Work do
       expect(books.first).must_equal works(:wheel_2)
 
       i = 0
-      while i < books.length - 2
+      while i < books.length - 1
         expect(books[i].votes.count).must_be :>=, books[i + 1].votes.count
         i += 1
       end  
@@ -103,7 +103,6 @@ describe Work do
       expect(books.first.votes.count).must_equal 3
       expect(books.first.votes.count).must_equal 3
       expect(books.first.id).must_be :>, books.second.id
-
     end
 
     it "raises an exception if category is not allowed" do
@@ -134,9 +133,22 @@ describe Work do
     end
 
     it "returns a collection sorted by rating in descending order" do
-      # TODO
+      books = Work.top_ten("book")
+      expect(books.first).must_equal works(:wheel_2)
+
+      i = 0
+      while i < books.length - 1
+        expect(books[i].votes.count).must_be :>=, books[i + 1].votes.count
+        i += 1
+      end  
     end
 
+    it "orders a collection by id in case of a tie between votes" do
+      books = Work.top_ten("book")
+      expect(books.first.votes.count).must_equal 3
+      expect(books.first.votes.count).must_equal 3
+      expect(books.first.id).must_be :>, books.second.id
+    end
     it "raises an exception if category is not allowed" do
       expect {
         Work.top_ten("video-game")
@@ -161,8 +173,26 @@ describe Work do
       expect(Work.spotlight).must_be_nil
     end
 
-    it "returns a work with a highest rating" do
-      # TODO
+    it "returns a work with the highest rating" do
+      spotlight = Work.spotlight
+      works = Work.all
+
+      i = 0
+      while i < works.length
+        expect(works[i].votes.count).must_be :<=, spotlight.votes.count
+        i += 1
+      end  
+    end
+
+    it "returns a work with a higher is in case of a tie in votes" do
+      spotlight = Work.spotlight
+      expect(spotlight).must_equal works(:wheel_2)
+
+      works = Work.all
+      highest_votes = works.select{ |work| work.votes.count == spotlight.votes.count }
+      expect(highest_votes.length).must_equal 2
+      expect(highest_votes).must_include works(:wheel_1)
+      expect(spotlight.id).must_be :>, works(:wheel_1).id
     end
   end
 end
